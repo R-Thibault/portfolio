54d2ac7cc395af0b760de22b1c437669f3353744
Thibault R <44661431+R-Thibault@users.noreply.github.com>
2023-05-11 09:54:52 +0200

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Analytics } from '@vercel/analytics/react';
+ 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
